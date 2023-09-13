e89d74bbb40783c5d8a574790c2ada245bf07706
lidorg-dev <lidorg@dev-operations.io>
2023-09-13 16:46:47 +0300

    added all sed

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export MOVIE_API_KEY=""
-export SECRET_KEY=""
-export MAIL_USERNAME=""
-export MAIL_PASSWORD=""
+export MOVIE_API_KEY=
+export SECRET_KEY=
+export MAIL_USERNAME=
+export MAIL_PASSWORD=
 
 python manage.py runserver
